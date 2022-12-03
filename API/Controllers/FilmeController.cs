27d74571012b391c1d7274ae48c249dd19821b34
Rian Macedo <rian.macedo@e-gestao.com.br>
2022-12-02 22:21:56 -0300

    adicionando arquivos iniciais

---
+++ API/Controllers/FilmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static int currentId = 1;
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public IActionResult Adicionar([FromBody]Filme filme) {
+            filme.Id = currentId++;
+            filmes.Add(filme);
+
+            return CreatedAtAction(nameof(BuscaPorId), new { Id = filme.Id }, filme);
+        }
+        [HttpGet]
+        public IActionResult Buscar() {
+            return Ok(filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscaPorId(int id) {
+            Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+
+            if(filme != null)
+            {
+               return Ok(filme);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ API/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Necessário adicionar titulo no filme")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Necessário adicionar diretor no filme")]
+        public string Diretor { get; set; }
+        [StringLength(100, ErrorMessage = "Genero com tamanho máximo de 100 caracteres")]
+        public string Genero {  get ; set;  }
+        [Range(1, 600)]
+        public int Duracao { get; set; }
+    }
+}
